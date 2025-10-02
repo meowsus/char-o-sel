1f07abe86bb1d2a2b8d3555e529fee6a058d7a1d
meowsus <tooblies@gmail.com>
2025-10-02 10:42:05 -0700

    Stub out files

---
+++ script.js
@@ -0,0 +1,15 @@
+class COSContainer extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+
+customElements.define("cos-container", COSContainer);
+
+class COSItem extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+
+customElements.define("cos-item", COSItem);
